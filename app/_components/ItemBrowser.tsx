e900c9557610c418d63ecedae41fb10a57c11fdb
Xavier Hillman <xhillman13@gmail.com>
2026-01-07 20:18:34 -0500

    basic item browser and item card layouts

--- app/_components/ItemBrowser.tsx
+++ app/_components/ItemBrowser.tsx
@@ -1,7 +1,17 @@
+import ItemCard from "./ItemCard";
+
 export default function ItemBrowser() {
   return (
     <div>
-      <h2>Item Browser</h2>
+      <h2 className="text-2xl font-bold mb-4">All Items</h2>
+      <div className="item-browser-grid grid grid-cols-3 gap-4">
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+      </div>
     </div>
   );
 }
--- app/_components/ItemCard.tsx
+++ app/_components/ItemCard.tsx
@@ -1,7 +1,17 @@
 export default function ItemCard() {
   return (
-    <div>
-      <h2>Item Card</h2>
+    <div className="item-card bg-neutral-800 p-4">
+      <div>
+              <h2>Item Name</h2>
+              <p>8 oz</p>
+
+      </div>
+      <div className="item-card-footer flex justify-between items-center">
+        <p>$10</p>
+        <button>Add</button>
+      </div>
+
+
     </div>
   );
 }
